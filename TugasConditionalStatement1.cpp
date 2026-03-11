898b9e4e61ed783003de97ce7810c76e7cf5c3b0
almaudud <almadudiakunngoding@gmail.com>
2026-03-11 12:30:20 +0700

    menambahkan string status BMI

--- TugasConditionalStatement1.cpp
+++ TugasConditionalStatement1.cpp
@@ -4,3 +4,16 @@ using namespace std;
 float Rata_rata(float a, float b){
      return (a/(b*b));
 }
+
+string status_BMI(float r){
+    if (r <= 18.5)
+        return "Berat Badan Kurang ";
+    else if (r <= 25)
+        return "Berat Badan Normal";
+    else if (r <= 30)
+        return "Berat badan belebihan";
+    else if (r >=18.5)
+        return "Obesitas";
+    else if (r >= 30)
+        return "Obesitas";
+}
